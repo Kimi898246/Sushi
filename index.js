99e5512fd7ac8668cd09676c9d72bf8de0813227
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-05-25 12:35:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,107 +5,6 @@ const client = new discord.Client({
 });
 const { MessageMenuOption, MessageMenu } = require("discord-buttons");
 const { MessageEmbed } = require('discord.js');
-
-client.on('message', async message => {
-  if(message.content == "s-menu") {
-    let option1 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option2 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-        
-    let option3 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option4 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option5 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option6 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option7 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-
-    let option8 = new MessageMenuOption()
-      .setLabel("Option 1")
-      .setValue("Option 2")
-      .setDescription("this will give u option 1")
-    
-    let selection = new MessageMenu()
-      .setID("Selection")
-      .setMaxValues(1)
-      .setMinValues(1)
-      .setPlaceHolder("Click me to make a Selection | PONG")
-      .addOption(option1)
-      .addOption(option2)
-      .addOption(option3)
-      .addOption(option4)
-      .addOption(option5)
-      .addOption(option6)
-      .addOption(option7)
-      .addOption(option8)
-
-    let menumsg = await message.channel.send("something", selection)
-
-    function menuselection(menu) {
-      switch(menu.values[0]) {
-        case "Option 1":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 2":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 3":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 4":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 5":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 6":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 7":
-          menu.reply.send("reply for option 1", true)
-        break;
-        case "Option 8":
-          menu.reply.send("reply for option 1", true)
-        break;
-        default:
-          menu.reply.send("This is not your menu", true)
-        break;
-      }
-    }
-    
-    client.on("clickMenu", (menu) => {
-      if(menu.message.id == menumsg.id) {
-        if(menu.clicker.user.id == message.author.id) menuselection(menu)
-        else menu.reply.send("This is not your menu", true)
-      }
-    })
-    
-  }
-});
-
 const express = require('express')
 const app = express();
 const port = 3000;
@@ -116,8 +15,6 @@ app.listen(port, () =>
 console.log(`Your app is listening a http://localhost:${port}`)
 );
 require("dotenv").config();
-
-"$TOKEN";
 const guildDB = require("./mongo/guildDB");
 const { default_prefix } = require("./config.json");
 const fetch = require("node-fetch");
@@ -175,148 +72,6 @@ client.on("message", async message => {
   if (command) command.run(client, message, args);
 });
 
-client.on("guildMemberAdd", async (member) => {
-  try {
-     console.log('test')
-    let data = await guildDB.find({ guild: member.guild.id });
-    var channel = data.map((guildDB) => {
-        return [ `${guildDB.channel}` ]})
-        console.log(channel)
-    if (!channel) return console.log('no channel')
-    console.log('test')
-    let message = data.map((guilDB) => { return [ `${guildDB.message}` ]});
-    console.log('test')
-    if (!message) message = "[member:mention] Welcome to [guild:name]";
-    console.log(channel)
-    console.log(message)
-    let guildCh = client.guilds.cache.get(member.guild.id)
-    let f = await guildCh.channels.cache.get(channel).send(message);
-    console.log(f)
-    setTimeout(async () => {
-      await f.delete();
-    }, 1000);
-  client.channels.cache.get(chx).send("A random nerd appeared, welcome " + member.user.username, attachment);
-  } catch (e) {
-    console.log(e)
-  }
-});
-
-  client.on("guildMemberAdd", async (member) => {
-      let LoggingChannel = await db.get(`LoggingChannel_${member.guild.id}`);
-  if (!LoggingChannel)return console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} Guild (channel not found)`);
-
-  //getting notify role
-  let notifyRole = await db.get(`notifyRole_${member.guild.id}`);
-  if (!notifyRole)return console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} Guild (role not found)`);
-
-  //to get created date in days format
-  let x = Date.now() - member.user.createdAt;
-  let created = Math.floor(x / 86400000);
-
-  //creation date
-  let creationDate = moment
-    .utc(member.user.createdAt)
-    .format("dddd, MMMM Do YYYY, HH:mm:ss");
-
-  //joindate
-  let joiningDate = moment
-    .utc(member.joinedAt)
-    .format("dddd, MMMM Do YYYY, HH:mm:ss");
-
-  //joinposition
-  let joinPosition = member.guild.memberCount
-
-  //altdate
-  let AltAge = await db.get(`altAge_${member.guild.id}`)
-  if (!AltAge) return db.set(`altAge_${member.guild.id}`, 31)
-
-  //only sends message when alt found
-  if (created < AltAge) {
-    //embed
-    let altEmbed = //main alt message
-    new Discord.MessageEmbed().setTitle("Alt Found!").setColor("RANDOM").setFooter("Bot Made By ItzCutePihcu#0001")
-      .setDescription(`
-**__Alt Name__**: ${member.user} (${member.user.username})
-**__ID__**: ${member.user.id}
-**__Account Created__**: ${created} days ago
-**__Account Creation Date__**: ${creationDate}
-**__Join Position__**: ${joinPosition}
-**__Join Date__**: ${joiningDate}
-`);
-
-member.guild.channels.cache.get(LoggingChannel).send(`__Notification:__ <@&${notifyRole}>`, altEmbed);
-
-
-let AutoKick = await db.fetch(`AutoKick_${member.guild.id}`);
-if (!AutoKick)return console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} Guild (AutoKick Isn't Enabled)`);
-
-let AutoKickAge = await db.get(`AutokickAge_${member.guild.id}`)
-if (!AutoKickAge) return db.set(`AutokickAge_${member.guild.id}`, 8)
-
-  if (AutoKick === true) {
-
- let checking = await db.get(`WhiteListed_${member.guild.id}`)
-
-  if (checking === member.user.id) {
-   let embed = new Discord.MessageEmbed()
-   .setTitle(`Auto Kick System Stucked On`)
-   .setDescription(`
-**__NAME__** - ${member.user} (${member.user.username})
-**__KICKED__** - NO
-**__REASON__** - WhiteListed User`)
-.setColor("RANDM");
-member.guild.channels.cache.get(LoggingChannel).send(embed)
-
-  } else {
-
-    if (created < AutoKickAge) {
-    let embed = new Discord.MessageEmbed()
-    .setTitle(`Auto Kick System Kicked SomeOne`)
-    .setDescription(`
-**__NAME__** - ${member.user} (${member.user.username})
-**__ID__** - ${member.user.id}
-**__KICKED FROM GUILD NAME__** - ${member.guild.name}
-**__KICKED REASON__** - ALT ( Created ${created} Days Ago)
-`)
-    .setColor('RANDOM')
-      member.kick()
-      console.log(`kicked`)
-      member.guild.channels.cache.get(LoggingChannel).send(embed)
-
-  } 
-}
-
-  } else {
-    console.log(`Autokick Isnt Disabled in ${memeber.guild.name}`)
-
-  }
-
-   }
-  }
-  )
-
-
-
-
-
-client.on("message", async message => {
-if (message.channel.name == "chatbot") {
-if (message.author.bot) return;
-message.content = message.content.replace(/@(everyone)/gi, "everyone").replace(/@(here)/gi, "here");
-if (message.content.includes(`@`)) {
-return message.channel.send(`**:x: Please dont mention anyone**`);
- }
-  message.channel.startTyping();
-if (!message.content) return message.channel.send("Please say something.");
-fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${client.user.username}&ownername=npg`)
-    .then(res => res.json())
-    .then(data => {
-        message.channel.send(`> ${message.content} \n <@${message.author.id}> ${data.message}`);
-    });
-      message.channel.stopTyping();
-}
-});
-
 client.snipes = new Map()
 client.on('messageDelete', function(message, channel){
   
@@ -364,11 +119,11 @@ const fs = require('fs')
 
  client.on('guildCreate', guild =>{
 
-    const channelId = '970054253525733386'; //put your channel ID here
+    const channelId = '970054253525733386';
 
     const channel = client.channels.cache.get(channelId); 
      
-    if(!channel) return; //If the channel is invalid it returns
+    if(!channel) return; 
     const embed = new discord.MessageEmbed()
         .setTitle('I Joined A Guild!')
         .setDescription(`**Guild Name:** ${guild.name} (${guild.id})\n**Members:** ${guild.memberCount}`)
@@ -380,10 +135,10 @@ const fs = require('fs')
 
 
 client.on('guildDelete', guild =>{
-    const channelId = '841994754399928341';//add your channel ID
-    const channel = client.channels.cache.get(channelId); //This Gets That Channel
+    const channelId = '970054253525733386';
+    const channel = client.channels.cache.get(channelId); 
     
-    if(!channel) return;  //If the channel is invalid it returns
+    if(!channel) return; 
     const embed = new discord.MessageEmbed()
         .setTitle('I Left A Guild!')
         .setDescription(`**Guild Name:** ${guild.name} (${guild.id})\n**Members:** ${guild.memberCount}`)
@@ -439,7 +194,7 @@ var code = '';
 
 client.on('message', message => {
  if (message.content.startsWith('s-say ')) {
-  code = message.content.substr(6); // Remove first 6 characters of message
+  code = message.content.substr(6);
   message.delete()
   message.channel.send(code);
  }
